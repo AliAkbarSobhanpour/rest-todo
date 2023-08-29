cd4d23dd07aae5299dbac5c5c7f38c7c58c70b43
AliAkbarSobhanpour <a.a.s.javid0416@gmail.com>
2023-08-29 16:21:45 +0330

    traditional django todo app created fully

--- todos/tests.py
+++ todos/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
-
+from .models import Todo
 # Create your tests here.
+
+
+class TodoModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.todo = Todo.objects.create(
+            title = "this is title",
+            body = "this is body"
+        )
+    
+
+    def test_model_content(self):
+        self.assertEqual(self.todo.title, "this is title")
+        self.assertEqual(self.todo.body, "this is body")
+        self.assertEqual(str(self.todo), "this is title")
