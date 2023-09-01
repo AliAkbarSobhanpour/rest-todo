fe32665e93855ed2d03712d0f1b62b3c50d4a100
AliAkbarSobhanpour <a.a.s.javid0416@gmail.com>
2023-09-02 01:03:11 +0330

    create Api endpoints for Get all and single data

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # internal apps
     "todos.apps.TodosConfig",
+
+    #external app 
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -123,3 +126,11 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+    ],
+}
